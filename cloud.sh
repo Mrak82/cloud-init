90a9f61e7cbbb737c328adfc402c071776cf28e2
root <root@pve-01.lan>
2024-06-21 09:13:33 +0300

    first commit

---
+++ cloud.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+export IMAGES_PATH="/var/lib/vz/images/9999" # defines the path where the images will be stored and change the path to it.
+
+cd ${IMAGES_PATH}
+
+## Ubuntu 23.04 Lunar Lobster
+## https://cloud-images.ubuntu.com/
+# wget https://cloud-images.ubuntu.com/lunar/current/lunar-server-cloudimg-amd64.img
+
+export QEMU_CPU_MODEL="host" # Specifies the CPU model to be used for the VM according your environment and the desired CPU capabilities.
+export VM_CPU_SOCKETS=1
+export VM_CPU_CORES=4
+export VM_MEMORY=4098
+export VM_STORAGE="local" # Assigns the VM to a specific resource pool for management.
+
+export CLOUD_INIT_USER="root" # Specifies the username to be created using Cloud-init.
+export CLOUD_INIT_SSHKEY="/root/.ssh/id_rsa.pub" # Provides the path to the SSH public key for the user.
+export CLOUD_INIT_IP="dhcp"
+#export CLOUD_INIT_NAMESERVER="192.168.1.1"
+#export CLOUD_INIT_SEARCHDOMAIN=".lan"
+
+export TEMPLATE_ID=9999
+export VM_NAME="debian12-cloud"
+export VM_DISK_IMAGE="${IMAGES_PATH}/debian-12-generic-amd64.qcow2"
+
+####
+
+# Create VM. Change the cpu model
+qm create ${TEMPLATE_ID} --name ${VM_NAME} --cpu ${QEMU_CPU_MODEL} --sockets ${VM_CPU_SOCKETS} --cores ${VM_CPU_CORES} --memory ${VM_MEMORY} --numa 1 --net0 virtio,bridge=vmbr0 --ostype l26 -machine q35 --agent 1 --scsihw virtio-scsi-single
+
+# Start at boot
+qm set ${TEMPLATE_ID} -onboot 1
+
+# Import Disk
+qm set ${TEMPLATE_ID} --scsi0 ${VM_STORAGE}:0,import-from=${VM_DISK_IMAGE}
+
+# Add Cloud-Init CD-ROM drive. This enables the VM to receive customization instructions during boot.
+qm set ${TEMPLATE_ID} --ide2 ${VM_STORAGE}:cloudinit --boot order=scsi0
+
+# Cloud-init network-data
+qm set ${TEMPLATE_ID} --ipconfig0 ip=${CLOUD_INIT_IP} #--nameserver ${CLOUD_INIT_NAMESERVER} --searchdomain ${CLOUD_INIT_SEARCHDOMAIN}
+
+# Cloud-init user-data
+qm set ${TEMPLATE_ID} --ciupgrade 1 --ciuser ${CLOUD_INIT_USER} --sshkeys ${CLOUD_INIT_SSHKEY}
+
+#Resize the disk to 50G
+qm disk resize ${TEMPLATE_ID} scsi0 10G
+
+# Cloud-init regenerate ISO image, ensuring that the VM will properly initialize with the desired parameters.
+qm cloudinit update ${TEMPLATE_ID}
+
+#####
+
+qm set ${TEMPLATE_ID} --name "${VM_NAME}-Template"
+
+qm template ${TEMPLATE_ID}
